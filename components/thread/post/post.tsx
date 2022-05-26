31c7530fff91dfb4e5e157799c92443b4231335e
jaewoo_byeon2 <jaewoo_byeon2@tmax.co.kr>
2022-05-26 10:29:42 +0900

    포스트 디자인 수정 Fixes #8

--- components/thread/post/post.tsx
+++ components/thread/post/post.tsx
@@ -7,21 +7,26 @@ type PostContainerProps = {
 const PostContainer = ({ post }: PostContainerProps) => {
   const createdAt = dayjs(post.created_at)
     .locale("ko")
-    .format("YYYY-MM-DD a hh:mm");
+    .format("YY-MM-DD HH:mm");
 
   return (
     <article className="flex flex-col rounded bg-white p-4">
-      <div className="flex flex-row gap-2">
-        <Link href={`#${post.number}`}>
-          <a
-            className="whitespace-pre text-teal-500 hover:underline"
-            id={String(post.number)}
-          >
-            &gt;&gt;{post.number}
-          </a>
-        </Link>
-        <span className="text-teal-700">{post.username}</span>
-        <span className="text-teal-700">{createdAt}</span>
+      <div className="flex flex-row justify-between">
+        <div className="flex flex-row gap-2">
+          <Link href={`#${post.number}`}>
+            <a
+              className="whitespace-pre text-teal-500 hover:underline"
+              id={String(post.number)}
+            >
+              &gt;&gt;{post.number}
+            </a>
+          </Link>
+          <span className="text-teal-700">{post.username}</span>
+          <span className="text-teal-700">{createdAt}</span>
+        </div>
+        <div className="text-teal-500">
+          <span>#{post.id}</span>
+        </div>
       </div>
       <p
         className="whitespace-pre-wrap text-teal-900"
