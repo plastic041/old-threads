4decca6aa496a774d2451b71b01c20f5dfc97583
jaewoo_byeon2 <jaewoo_byeon2@tmax.co.kr>
2022-05-26 16:39:00 +0900

    [feat] 서버모킹안함

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -2,25 +2,6 @@ import "../styles/globals.css";
 import type { AppProps } from "next/app";
 import "dayjs/locale/ko";
 
-const isServer = typeof window === "undefined";
-
-// call msw
-if (process.env.NODE_ENV === "test") {
-  if (isServer) {
-    const callMsw = async () => {
-      const { server } = await import("~/src/mocks/server");
-      server.listen();
-    };
-    callMsw();
-  } else {
-    const callMsw = async () => {
-      const { worker } = await import("~/src/mocks/browser");
-      worker.start();
-    };
-    callMsw();
-  }
-}
-
 function MyApp({ Component, pageProps }: AppProps) {
   return (
     <div className="container mx-auto">
